2303ac165f1292eb2cbaa7dc43e74321815f3cc4
vitorGPorto <vitorgsporto01@gmail.com>
2021-04-26 21:10:10 -0300

    Primeiro commit

---
+++ script.js
@@ -0,0 +1,50 @@
+window.addEventListener("load", function () {
+    var submitButton = document.querySelector('#app form button');
+    var zipCodeField = document.querySelector('#app form input');
+    console.log('teste ' + zipCodeField)
+    var content = document.querySelector('#app main');
+
+
+    submitButton.addEventListener('click', run)
+
+    function run(event) {
+        event.preventDefault()
+        var zipCode = zipCodeField.value
+        zipCode = zipCode.replace(' ', '')
+        zipCode = zipCode.replace('.', '') 
+        zipCode = zipCode.trim() //para remover o espaço
+
+
+        console.log(zipCode)
+
+        axios.get('https://viacep.com.br/ws/'+ zipCode + '/json/')
+        .then(function (response){
+
+            if (response.data.erro){
+                throw new Error('CEP inválido')
+            }
+
+            content.innerHTML = ''
+            createLine(response.data.logradouro)
+            createLine(response.data.localidade + '/' + response.data.uf) 
+            createLine(response.data.bairro)
+        })
+        .catch(function (error){
+            content.innerHTML = ''
+            createLine('Ops, algo deu errado, verifique as informações!')
+        })
+
+        function createLine(text){
+              var line = document.createElement('p')
+            var  text = document.createTextNode(text)
+            line.appendChild(text)
+            content.appendChild(line)
+
+        }
+
+      
+    }
+
+
+})
+//34580-530
